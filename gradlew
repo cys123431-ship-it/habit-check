3fc2d99e2388bed1091ea3880d7a1f29e94f906c
cys123431-ship-it <cys123431@gmail.com>
2026-02-20 15:12:32 +0900

    Add gradle wrapper fallback to local gradle

--- gradlew
+++ gradlew
@@ -1,5 +1,10 @@
 ﻿#!/usr/bin/env sh
 DEFAULT_JVM_OPTS=""
 APP_HOME=`dirname "$0"`
-CLASSPATH="$APP_HOME/gradle/wrapper/gradle-wrapper.jar"
-exec java $DEFAULT_JVM_OPTS -classpath "$CLASSPATH" org.gradle.wrapper.GradleWrapperMain "$@"
+WRAPPER_JAR="$APP_HOME/gradle/wrapper/gradle-wrapper.jar"
+
+if [ -f "$WRAPPER_JAR" ]; then
+  exec java $DEFAULT_JVM_OPTS -classpath "$WRAPPER_JAR" org.gradle.wrapper.GradleWrapperMain "$@"
+else
+  exec gradle "$@"
+fi
--- gradlew.bat
+++ gradlew.bat
@@ -3,6 +3,10 @@ setlocal
 set DIR=%~dp0
 set APP_HOME=%DIR%
 set CLASSPATH=%APP_HOME%gradle\wrapper\gradle-wrapper.jar
-set JAVA_EXE=java
-if exist "%JAVA_HOME%\bin\java.exe" set JAVA_EXE=%JAVA_HOME%\bin\java.exe
-%JAVA_EXE% -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
+if exist "%CLASSPATH%" (
+    set JAVA_EXE=java
+    if exist "%JAVA_HOME%\bin\java.exe" set JAVA_EXE=%JAVA_HOME%\bin\java.exe
+    %JAVA_EXE% -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
+) else (
+    gradle %*
+)
